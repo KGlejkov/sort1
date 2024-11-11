ae160e5f236c1eb18e85f3dea787a91ce6524b73
KGlejkov <gklejkov@gmail.com>
2024-11-11 09:47:28 +0700

    Update sort1.py

--- sort1.py
+++ sort1.py
@@ -43,28 +43,26 @@ def partition(mas,low,high):
             i+=1
     mas[low],mas[i-1] = mas[i-1],mas[low]
     return i-1
-#Ниже - bogosort
-import random
-def sorted_(a):
-    lenl = len(a)
-    for i in range(0, lenl - 1):
-        if(a[i] > a[i + 1]):
-            return False
-    return True
-def random(a):
-    lenl = len(a)
-    for i in range(0, lenl):
-        rnd = random.randint(0, lenl - 1)
-        temp = a[i]
-        a[i] = a[rnd]
-        a[rnd] = temp  
-def bogosort(a):
-    while(not(sorted_(a))):
-        random_permutation(a)
-mas = []
-n = int(input("Длина массива:")) 
-for i in range(0, n): 
-    elt = int(input("arr[" + str(i + 1) + "] = "))   
-    mas.append(elt)
-bogosort(mas) 
-print(mas)
+    
+def cocktailsort(a):
+    n=len(a)
+    swapp=True
+    e=n-1
+    s=0
+    e=n-1
+    while swapp==True:
+        swapp=False
+        for i in range(s,e):
+            if a[i] > a[i+1] :
+                a[i], a[i+1]= a[i+1], a[i]
+                swapp=True
+        if swapp==False:
+            break
+        swapp=False
+        e=e-1
+        for i in range(e-1, s-1,-1):
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                swapp=True
+        s=s+1
+    return a
